7cd3227971f54d7e15157086585c5bc285084cf0
Danish Malik <cyanide9102@gmail.com>
2026-05-06 05:41:22 +0300

    added identity-service for implementing jwt based security

---
+++ IdentityService/src/main/java/com/cyanide9102/identityservice/role/Role.java
@@ -0,0 +1,23 @@
+package com.cyanide9102.identityservice.role;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
---
+++ IdentityService/src/main/java/com/cyanide9102/identityservice/user/User.java
@@ -0,0 +1,36 @@
+package com.cyanide9102.identityservice.user;
+
+import com.cyanide9102.identityservice.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
